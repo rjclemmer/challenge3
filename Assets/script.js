04f1ac80dc483d36e23e722f68c6898142c9e4c1
Squirrely Rob <clemmer.robert.j@gmail.com>
2022-11-23 12:30:14 -0500

    connected index with JS and CSS. Added prompt and confirm methods

--- Assets/script.js
+++ Assets/script.js
@@ -5,34 +5,48 @@ var generateBtn = document.querySelector("#generate");
 
 
 
-// Write password to the #password input
+// // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  // prompts for password criteria
-// Uppercase
+  // length of passwork between 8 and 128
+  prompt("How many characters do you want in your password? Password length needs to be between 8 and 128 characters.")
+
+
+  confirm("Do you want to include Uppercase characters to your password?");
+
+  confirm("Do you want to include Lowercase characters to your password?");
+  prompt("Do you want to include Numbers to your password?");
+  prompt("Do you want to include various Symbols to your password?");
+  
+  // var input = value.prompt;
+  // console.log(input)
+}
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   // prompts for password criteria
+// // Uppercase
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
 "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-// lowercase
+// // lowercase
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
 "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-// numbers
+// // numbers
 var numbers =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-// symbols
+// // symbols
 var symbols = ['~','!','@','#','$','%','^','&','*','(',')','_','-','+','=','{','}','[',']','|','/','?','<','>',',']
 
-confirm("test test")
-alert("test")
-prompt("yes?")
+// confirm("test test")
+// alert("test")
+// prompt("yes?")
 
 
-passwordText.value = password;
+// passwordText.value = password;
 
-}
+// }
 
 
 
